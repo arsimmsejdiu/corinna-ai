1a3bf76d5d8d54f04aebd9474b921063c053240a
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-06-05 15:05:50 +0200

    Added custom dark mode toggle to settigns page

--- src/hooks/settings/use-settings.ts
+++ src/hooks/settings/use-settings.ts
@@ -1,14 +1,61 @@
 import {
-    onChatBotImageUpdate,
-    onCreateFilterQuestions,
-    onCreateHelpDeskQuestion,
-    onCreateNewDomainProduct,
-    onDeleteUserDomain,
-    onGetAllFilterQuestions,
-    onGetAllHelpDeskQuestions,
-    onUpdateDomain,
-    onUpdatePassword,
-    onUpdateWelcomeMessage,
-  } from '@/actions/settings'
-  import { useToast } from '@/components/ui/use-toast'
-  
+  onChatBotImageUpdate,
+  onCreateFilterQuestions,
+  onCreateHelpDeskQuestion,
+  onCreateNewDomainProduct,
+  onDeleteUserDomain,
+  onGetAllFilterQuestions,
+  onGetAllHelpDeskQuestions,
+  onUpdateDomain,
+  onUpdatePassword,
+  onUpdateWelcomeMessage,
+} from "@/actions/settings";
+import { useToast } from "@/components/ui/use-toast";
+import { ChangePasswordProps } from "@/constants/types";
+import { ChangePasswordSchema } from "@/schemas/auth.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useTheme } from "next-themes";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export const useThemeMode = () => {
+  const { setTheme, theme } = useTheme();
+  return {
+    setTheme,
+    theme,
+  };
+};
+
+export const useChangePassword = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ChangePasswordProps>({
+    resolver: zodResolver(ChangePasswordSchema),
+    mode: "onChange",
+  });
+  const { toast } = useToast();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onChangePassword = handleSubmit(async (values) => {
+    try {
+      setLoading(true);
+      const updated = await onUpdatePassword(values.password);
+      if (updated) {
+        reset();
+        setLoading(false);
+        toast({ title: "Success", description: updated.message });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  return {
+    register,
+    errors,
+    onChangePassword,
+    loading,
+  };
+};
